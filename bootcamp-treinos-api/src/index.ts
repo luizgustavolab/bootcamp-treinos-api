fbc33d421194f56e14ac0ea1768a690893ec1029
Luiz Gustavo <luizgustavolab@gmail.com>
2026-03-28 09:38:58 -0300

    feat: add fastify api

--- bootcamp-treinos-api/src/index.ts
+++ bootcamp-treinos-api/src/index.ts
@@ -1,1 +1,15 @@
-console.log ("Hello, World")
+import Fastify from "fastify";
+const fastify = Fastify({  
+    logger: true,
+});
+
+fastify.get("/", async function handler () {  
+    return { hello: "world" }
+});
+
+try {  
+    await fastify.listen({ port: Number(process.env.PORT)});
+} catch (err) { 
+     fastify.log.error(err);  
+    process.exit(1);
+}
